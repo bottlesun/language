d526818678c1e01e31d32438f11f164f52f0344e
bottlesun <bottlesun3514@gmail.com>
2022-08-17 07:32:47 +0900

    await 연달아 쓰기 금지

--- 05-js-engine/day02-4-5-async_promise.js
+++ 05-js-engine/day02-4-5-async_promise.js
@@ -3,23 +3,23 @@
 let a = 2;
 // Promise 란, 실행은 바로 하되, 결괏값을 나중에 원할 때 쓸 수 있는 것
 const p = new Promise((resolve, reject) => {
-  // Promise 안에 함수가 선언 되고 p 에 대입이 되고 밑으로 넘어간다.
-  // 해당 내부 함수는 동기 코드 이다.
-  console.log('제일먼저') // 호출 순서 1
+    // Promise 안에 함수가 선언 되고 p 에 대입이 되고 밑으로 넘어간다.
+    // 해당 내부 함수는 동기 코드 이다.
+    console.log('제일먼저') // 호출 순서 1
 
-  setTimeout(() => {
-    // 정상적인 흐름에서 바뀌어 있을때, 비동기
-    a = 5;
-    console.log(a); // 호출 순서 3
-    resolve(a);
-  }, 0)
+    setTimeout(() => {
+        // 정상적인 흐름에서 바뀌어 있을때, 비동기
+        a = 5;
+        console.log(a); // 호출 순서 3
+        resolve(a);
+    }, 0)
 });
 
 // 딴짓딴짓
 console.log('딴짓'); // 호출 순서 2
 
 p.then((result) => {
-  console.log('result', result) // // 호출 순서 4
+    console.log('result', result) // // 호출 순서 4
 });
 
 // 호출분석
@@ -52,34 +52,34 @@ p.then((result) => {
 
 /* 2-5 async/await Promise 로 바꾸기 */
 p.then((result) => {
-  console.log('result', result)
-  // return 1 -> 일반 값을 리턴 시 해당 값이 다음으로 넘어간다.
-  return Promise.resolve(1) // Promise 를 리턴하면 resolve 된 값이 다음으로 넘어간다.
+    console.log('result', result)
+    // return 1 -> 일반 값을 리턴 시 해당 값이 다음으로 넘어간다.
+    return Promise.resolve(1) // Promise 를 리턴하면 resolve 된 값이 다음으로 넘어간다.
 }).then((result) => {
-  console.log(result) // 1
-  return undefined;
+    console.log(result) // 1
+    return undefined;
 }).then((result) => {
-  // 함수는 return 이 없으면 undefined 이기에 undefined
-  console.log(result) // undefined
+    // 함수는 return 이 없으면 undefined 이기에 undefined
+    console.log(result) // undefined
 }).then(() => {
 
 }).catch(() => {
-  // 에러로 이동
+    // 에러로 이동
 }).finally(() => {
-  // then , catch 와 상관 없이 실행
+    // then , catch 와 상관 없이 실행
 });
 
 async function as() {
-  // 분석 시 async 함수의 끝은 await 가 시작 되기 전 이다. (동기부분)
-  const a = await 1; // await -> then
-  //const a = axios.get();
-  //const a = Promise; // promise 나 axios 같은 값들의 경우는 , 아래서 변경할때, 그냥 치환하여 사용이 가능하다. ex.1
-  console.log('a', a);
-  console.log('hmm');
-  await null;
-  const b = await Promise.resolve(1); // Promise.resolve 값을 넘겨준다.
-  console.log('b', b);
-  return b
+    // 분석 시 async 함수의 끝은 await 가 시작 되기 전 이다. (동기부분)
+    const a = await 1; // await -> then
+    //const a = axios.get();
+    //const a = Promise; // promise 나 axios 같은 값들의 경우는 , 아래서 변경할때, 그냥 치환하여 사용이 가능하다. ex.1
+    console.log('a', a);
+    console.log('hmm');
+    await null;
+    const b = await Promise.resolve(1); // Promise.resolve 값을 넘겨준다.
+    console.log('b', b);
+    return b
 }
 
 /* async/await Promise 로 바꾸는 규칙 ( 1 : 1 대응이 아님 )
@@ -103,9 +103,9 @@ Promise.resolve(1) // ex.1 ) axios.get().then()
   */
 
 a().then((result) => {
-  console.log(result)
+    console.log(result)
 }).then((result2) => {
-  console.log(result2)
+    console.log(result2)
 })
 
 
@@ -130,13 +130,68 @@ a().then((result) => {
 // M -
 // m - p1
 
-function delayP(ms) { // setTimeout 을 promis로 바꾸는 법
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms);
-    reject(error)
-  })
+function delayP(ms) { // setTimeout 을 Promise 로 바꾸는 법
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+        reject(error)
+    })
 };
 
-async  function a(){
-  await delayP(3000);
+async function AsyncA() {
+    await delayP(3000); // 3초
+    await delayP(6000); // 6초
+    await delayP(9000); // 9초
+} // 토탈 18초
+
+async function AsyncB() {
+    // 두개 이상을 동시에 실행 시키는 상황 일 경우 async 안에서도 promise 도 사용한다.
+    const p1 = delayP(3000);
+    const p2 = delayP(6000);
+    await promise.all([p1, p2]);//6초
+    await delayP(9000); // 9초
+} // 토탈 15초
+
+async function c() {
+    const a = await 1;
+    const b = await 2;
+
+    return a + b;
 }
+Promise.resolve(1)
+    .then((a) => {
+      return 2
+    })
+    .then((b) => {
+      return a + b
+    })
+
+    (function (){
+      let a ;
+      let b ;
+      return Promise.resolve(1)
+          .then((result) => {
+            a = result
+            return 2;
+          })
+          .then((result) => {
+            b = result
+            return a + b; // 시적 허용
+          })
+    })();
+
+async function createPost() {
+    const post = await db.getPost() // 게시물 조회;
+    if (post) {
+        res.status(403).send('이미 존재합니다.')
+    } else {
+        await db.createPost();  // 게시글 작성
+        //항상 동시에 진행 될 수 있는게 있는지 확인하기
+        const p1 = db.userIncrementPostCount(); // 사용자 게시글 카운트 1 올림
+        const p2 = db.createNoti(); // 새로운 게시글 알림 등록
+        await promise.allSettled([p1, p2]);
+    }
+}
+
+// Promise 란, 실행은 바로 하되, 결괏값을 나중에 원할 때 쓸 수 있는 것.
+// 실행은 바로 --> 결괏값이 나올 때는 나중에 --> 결괏 값이 사용 할 때는 더 나중
+// 실행은 바로 --> 결괏값도 바로 쓰고 싶은데 --> 그 다음에 결괏값이 나오면 --> then , await, promise.all 이런 결괏값을 기다린 후에 실행된다.
